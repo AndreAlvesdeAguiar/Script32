3018e87579205c54013f899c981e37cb4e74db96
AndreAlvesdeAguiar <aguiar1001@hotmail.com>
2024-11-02 14:40:41 -0300

    port

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+
+
 <img src="/img/aht10.png">
 
 <img src="/img/esp32.png">
---
+++ run_all.sh
@@ -0,0 +1,4 @@
+# #!/bin/bash
+# cd /home/andre/Documentos/py/Script_python && docker-compose up --build
+# cd /home/andre/Documentos/py/Script_go && docker-compose up --build
+# cd /home/andre/Documentos/py/Script_ruby && docker-compose up --build
